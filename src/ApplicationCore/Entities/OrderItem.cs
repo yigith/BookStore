194c46d47cf0a3b91de48b29f1d9037e3aa124a5
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-05-05 10:47:46 +0300

    Order Basket Migrations

--- src/ApplicationCore/Entities/OrderItem.cs
+++ src/ApplicationCore/Entities/OrderItem.cs
@@ -10,6 +10,8 @@ namespace ApplicationCore.Entities
     {
         public int ProductId { get; set; }
 
+        public int OrderId { get; set; }
+
         public string ProductName { get; set; }
 
         public decimal Price { get; set; }
