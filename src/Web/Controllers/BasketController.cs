eab5ac0109f2fd8d7b2f1ac4aa4807cdc72b9a3d
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-05-05 13:05:36 +0300

    basket links

---
+++ src/Web/Controllers/BasketController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Controllers
+{
+    // todo: web > basketviewmodelservice > getcreatebasket
+    // todo: appcore > basketservice > additemtobasket
+    public class BasketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
