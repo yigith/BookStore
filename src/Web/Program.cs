b3c0beb377d7486f4d2136b68456bb1e32eae8b2
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-05-17 11:19:10 +0300

    migrate on startup

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -2,6 +2,7 @@ using Infrastructure.Data;
 using Infrastructure.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,8 +23,11 @@ namespace Web
             using (var scope = host.Services.CreateScope())
             {
                 var appDbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                var appIdentityDbContext = scope.ServiceProvider.GetRequiredService<AppIdentityDbContext>();
                 var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
                 var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                await appDbContext.Database.MigrateAsync();
+                await appIdentityDbContext.Database.MigrateAsync();
                 await AppDbContextSeed.SeedAsync(appDbContext);
                 await AppIdentityDbContextSeed.SeedAsync(userManager, roleManager);
             }
